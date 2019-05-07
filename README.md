75715ecbd059e930a08d5a38c939986ed2680544
admin <linwenjun@163.com>
2019-05-07 02:35:40 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Spring Data JPA ORM 练习
+
+### 练习描述
+- 了解ORM的概念及特点
+- 在本地创建数据库并创建user表，其中包括id，name
+- 使用springboot jpa从数据库表生成User实体类
+
+### 环境描述
+- java8
+- Intellij-IDEA
+
+### 如何开始
+- 克隆模版库,在com.example.SpringDataJPAORM下完成需求
+
+### 输出规范
+- 在SpringDataJPAORM目录下生成User类
---
+++ src/main/java/com/example/SpringDataJPAORM/SpringDataJpaOrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataJPAORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaOrmApplication.class, args);
+	}
+
+}
